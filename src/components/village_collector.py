af2d185982b528afe515e7ecdeee1b999bfde9c8
Santiago Gómez Vilar <santi810@gmail.com>
2019-11-07 18:53:47 +0100

    Add dispersion in coordinates finder

--- src/components/village_collector.py
+++ src/components/village_collector.py
@@ -10,13 +10,41 @@ def remove_dipls(reference, removable):
     return removable
 
 
+def union(toret, otro):
+    for i in otro:
+        is_diferent = True
+        for j in toret:
+            if j == i:
+                is_diferent = False
+                break
+        if is_diferent:
+            toret.add(i)
+    return toret
+
+
 def find_villages(initial_coords):
     radius = 100
+    dispersion = 30
+    all_villages = find_villages_extended(initial_coords, radius)
+    extended_coords = [[initial_coords[0] + dispersion, initial_coords[1]],
+                       [initial_coords[0] - dispersion, initial_coords[1]],
+                       [initial_coords[0], initial_coords[1] + dispersion],
+                       [initial_coords[0], initial_coords[1] - dispersion],
+                       [initial_coords[0] - dispersion, initial_coords[1] - dispersion],
+                       [initial_coords[0] - dispersion, initial_coords[1] + dispersion],
+                       [initial_coords[0] + dispersion, initial_coords[1] - dispersion],
+                       [initial_coords[0] + dispersion, initial_coords[1] + dispersion]
+                       ]
+    for i in extended_coords:
+        query = find_villages_extended(i, radius)
+        all_villages = union(all_villages, query)
+
+    print(f"all: {len(all_villages)}")
+    return sorted(all_villages)
+
+
+def find_villages_extended(initial_coords, radius):
     normal_villages = get_farm_villages(initial_coords, radius, False)
     natare_villages = get_farm_villages(initial_coords, radius, True)
     natare_villages = remove_dipls(normal_villages, natare_villages)
-    all_villages = normal_villages.union(natare_villages)
-    print(f"villages: {len(normal_villages)}")
-    print(f"natare: {len(natare_villages)}")
-    print(f"all: {len(all_villages)}")
-    return sorted(all_villages)
+    return normal_villages.union(natare_villages)
